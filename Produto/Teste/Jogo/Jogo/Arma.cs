c9c7b2c980b1882cfe417a9c367cec4bd2d65313
malfardado <lucasribeiro20@hotmail.com>
2014-05-06 00:09:49 -0300

    Criação projeto de Testes
    Criação Projeto de Testes com classes Ficticias

---
+++ Produto/Teste/Jogo/Jogo/Arma.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo
+{
+    public class Arma
+    {
+        protected String Nome;
+        protected String tipo;
+        protected int qtdeMunicao;
+
+        public Boolean disparar()
+        {
+            if (tipo == "Faca" || qtdeMunicao <= 0)
+                return false;
+            else
+            {
+                qtdeMunicao--;
+                return true;
+            }
+
+
+        }
+    }
+    public class Revolver : Arma
+    {
+        public Revolver()
+        {
+            this.Nome = "Revolver 38";
+            this.tipo = "Revolver";
+            this.qtdeMunicao = 6;
+
+        }
+    }
+    public class Faca : Arma
+    {
+        public Faca()
+        {
+            this.Nome = "Peixeira";
+            this.tipo = "Faca";
+            this.qtdeMunicao = 0;
+        }
+       
+    }
+}
---
+++ Produto/Teste/Jogo/TesteJogo/TesteJogo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jogo;
+using NUnit.Framework;
+
+namespace TesteJogo
+{
+    [TestFixture]
+    public class TesteJogo
+    {
+        [Test]
+        public void TesteRevolver()
+        {
+            //Instacia de revolver
+            Revolver rev = new Revolver();
+            //verifica se o revolver disparou os 6 tiros
+            for (int i = 0; i < 6; i++)
+                Assert.IsTrue(rev.disparar());
+            //verifica que o revolver não atira mais quando está vazio
+            Assert.IsFalse(rev.disparar());
+        }
+
+        [Test]
+        public void TesteFaca()
+        {
+            Faca faca = new Faca();
+            //verifica que uma faca não pode disparar
+            Assert.IsFalse(faca.disparar());
+
+        }
+
+    }
+}
