d66925fa7a0d1dea13797454568055d10ad0f1e7
malfardado <lucasribeiro20@hotmail.com>
2014-05-30 22:46:03 -0300

    Criação Classe Potz
    implementada a Classe Potz e o projeto agora compila mas não passa no
    teste

---
+++ TP TDD/PROJETO/TddPotz/src/br/com/tdd/potz/Potz.java
@@ -0,0 +1,22 @@
+package br.com.tdd.potz;
+
+public class Potz {
+	private String cupom;
+	
+	public Potz (String cupom) {
+		this.cupom = cupom;
+	}
+
+	public String getCupom() {
+		return cupom;
+	}
+
+	public void setCupom(String cupom) {
+		this.cupom = cupom;
+	}
+	
+	public Boolean isValido(){
+		return false;
+	}
+
+}
