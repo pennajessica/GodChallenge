ee84e4cee4d016f0d418438c61ea6052033fe0bb
malfardado <lucasribeiro20@hotmail.com>
2014-06-01 22:38:11 -0300

    Criação Classe Mod11CK
    Criada a Classe Mod11CK. Caso de teste agora compila mas não passa

---
+++ TP TDD/PROJETO/TddPotz/src/br/com/util/Mod11Ck.java
@@ -0,0 +1,9 @@
+package br.com.util;
+
+public class Mod11Ck {
+	
+	public static Boolean isValido(String sequenciaNumerica) {
+		return true;
+	}
+
+}
--- TP TDD/PROJETO/TesteUnitario/src/br/com/tdd/util/Mod11CkTest.java
+++ TP TDD/PROJETO/TesteUnitario/src/br/com/tdd/util/Mod11CkTest.java
@@ -1,19 +1,20 @@
 package br.com.tdd.util;
 
+import br.com.util.Mod11Ck;
 import junit.framework.TestCase;
 
 public class Mod11CkTest extends TestCase {
 	
 	public void testeMod11Valido() throws Exception {
-		assertTrue(Mod11Ck.isValido("5001234560");//Potz de exemplo no enunciado
-		assertFalse(Mod11Ck.isValido("5001234561");
-		assertFalse(Mod11Ck.isValido("5001234562");
-		assertFalse(Mod11Ck.isValido("5001234563");
-		assertFalse(Mod11Ck.isValido("5001234564");
-		assertFalse(Mod11Ck.isValido("5001234565");
-		assertFalse(Mod11Ck.isValido("5001234566");
-		assertFalse(Mod11Ck.isValido("5001234567");
-		assertFalse(Mod11Ck.isValido("5001234568");
-		assertFalse(Mod11Ck.isValido("5001234569");
+		assertTrue(Mod11Ck.isValido("5001234560"));//Potz de exemplo no enunciado
+		assertFalse(Mod11Ck.isValido("5001234561"));
+		assertFalse(Mod11Ck.isValido("5001234562"));
+		assertFalse(Mod11Ck.isValido("5001234563"));
+		assertFalse(Mod11Ck.isValido("5001234564"));
+		assertFalse(Mod11Ck.isValido("5001234565"));
+		assertFalse(Mod11Ck.isValido("5001234566"));
+		assertFalse(Mod11Ck.isValido("5001234567"));
+		assertFalse(Mod11Ck.isValido("5001234568"));
+		assertFalse(Mod11Ck.isValido("5001234569"));
 	}
 }
