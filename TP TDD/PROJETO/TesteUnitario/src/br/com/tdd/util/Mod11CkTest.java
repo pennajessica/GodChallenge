b4e2217501ffcd5b3e9addadafcc5e41da74afb6
malfardado <lucasribeiro20@hotmail.com>
2014-06-01 22:26:39 -0300

    Novo Caso de Teste. Classe Utilitaria Para Validar Mod11
    Criado um caso de teste para uma função utilitária que irá verificar o
    dígito verificador.
    Caso de teste não compila pois a classe ainda não foi implementada.

---
+++ TP TDD/PROJETO/TesteUnitario/src/br/com/tdd/util/Mod11CkTest.java
@@ -0,0 +1,19 @@
+package br.com.tdd.util;
+
+import junit.framework.TestCase;
+
+public class Mod11CkTest extends TestCase {
+	
+	public void testeMod11Valido() throws Exception {
+		assertTrue(Mod11Ck.isValido("5001234560");//Potz de exemplo no enunciado
+		assertFalse(Mod11Ck.isValido("5001234561");
+		assertFalse(Mod11Ck.isValido("5001234562");
+		assertFalse(Mod11Ck.isValido("5001234563");
+		assertFalse(Mod11Ck.isValido("5001234564");
+		assertFalse(Mod11Ck.isValido("5001234565");
+		assertFalse(Mod11Ck.isValido("5001234566");
+		assertFalse(Mod11Ck.isValido("5001234567");
+		assertFalse(Mod11Ck.isValido("5001234568");
+		assertFalse(Mod11Ck.isValido("5001234569");
+	}
+}
